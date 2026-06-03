bd90b9da6144a5dec12f41fb863e6e5e6cbbc815
Joshua Hamilton <joshua@ungana.com>
2026-06-03 14:54:45 -0400

    Hello World module.

---
+++ hello-world/hello.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+const (
+	french  = "French"
+	spanish = "Spanish"
+
+	englishHelloPrefix = "Hello, "
+	frenchHelloPrefix  = "Bonjour, "
+	spanishHelloPrefix = "Hola, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	fmt.Println(Hello("World", ""))
+}
