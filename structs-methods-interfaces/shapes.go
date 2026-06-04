ff0a1a6bc75a9228c8cb50a94ab2d31dfd3ffc8f
Joshua Hamilton <joshua@ungana.com>
2026-06-03 21:30:47 -0400

    Structs, Methods, and Interfaces module.

---
+++ structs-methods-interfaces/shapes.go
@@ -0,0 +1,41 @@
+package main
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+func Area(r Rectangle) float64 {
+	return r.Width * r.Height
+}
+
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Width + r.Height)
+}
